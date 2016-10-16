6c9cd190e3841d56c92cb97ea29f440057e2022a
chuloon <ben.ng@outlook.com>
2016-10-16 00:58:34 -0400

    end of day. still stuck on getting firebase and angular working...

--- cosplarity-web/app.js
+++ cosplarity-web/app.js
@@ -1,4 +1,14 @@
 $().ready(function () {
+    $.getScript('https://www.gstatic.com/firebasejs/3.5.0/firebase.js', function () {
+        var config = {
+            apiKey: "AIzaSyD8Lnr7oH-QY97adMdhLUtKBOSQONGkMr0",
+            authDomain: "cosplarity.firebaseapp.com",
+            databaseURL: "https://cosplarity.firebaseio.com",
+            storageBucket: "cosplarity.appspot.com",
+            messagingSenderId: "159653646318"
+        };
+        firebase.initializeApp(config);
+    });
     $.get("../index/index.html", function (result) {
         $('#main-content').html(result);
     });
